57d8e46581162880f6cc6e26569e65c8e52574b0
Lokesh <lmlokesh43@gmail.com>
2019-08-24 10:29:40 +0100

    updating query in db_changes.sql file

--- db_changes.sql
+++ db_changes.sql
@@ -3,4 +3,4 @@
 ALTER TABLE `comm_templates` CHANGE `updated_at` `updated_at` TIMESTAMP on update CURRENT_TIMESTAMP NULL DEFAULT NULL; 
 
 /*24 aug 2019 sathish added*/
-ALTER TABLE `users` ADD `user_full_name` TEXT NULL DEFAULT NULL AFTER `nick_name` ;
+ALTER TABLE `users` ADD `full_name` TEXT NULL DEFAULT NULL AFTER `nick_name` ;
