43a7d4023ee26f22cd80d76b10fe4e6753a0d93a
LMLokesh <lokesh.mareppa@performgroup.com>
2019-08-31 03:57:41 +0100

    Dynamic form generation module implimentation continues.....

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -44,6 +44,7 @@
         <!--------Crop -->
 
         <link href="{{ URL::asset('css/main.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::asset('css/form_build.css') }}" rel="stylesheet" type="text/css" />
     </head>
 
     <body>
--- resources/views/settings/forms/create_edit.blade.php
+++ resources/views/settings/forms/create_edit.blade.php
@@ -2,97 +2,60 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-sm-12">
             <div class="text-white card-primary" >
                 <div class="card-body">
-                    <h3>Create New Form<small><a href="{{URL::asset('/settings/forms')}}" 
-                                class="btn btn-secondary pull-right">Forms List</a></small>
-                    </h3>
+                    <h6>Create New Form<a href="{{URL::asset('/settings/forms')}}" class="btn btn-secondary btn-sm pull-right">Forms List</a></h6>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row" style="margin-top:15px;">
+    <div class="clearfix"></div>
+    <div class="form_builder" style="margin-top: 25px">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <nav class="nav-sidebar">
+                            <h6 class="text-secondary" style="margin-bottom:0px;">Profile Fields</h6><hr>
+                            <ul class="nav profile_fields"></ul>
+                            <h6 class="text-secondary" style="margin-bottom:0px;">Basic Form Fields</h6><hr>
+                            <ul class="nav basic_fields"></ul>
+                        </nav>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">Form Title</span>
+                            </div>
+                            <input type="text" id="formTitle" class="form-control">
+                        </div>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">Description</span>
+                            </div>
+                            <input type="text" id="formTitle" class="form-control" >
+                        </div>
+                        <small><strong>First Name, Middle Name, Last Name and Email Address</strong> are always collected by every form.</small>
+                        <div class="row bal_builder">
+                            <div class="form_builder_area"></div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-md-5">
+                        <div class="col-md-12">
+                            <form class="form-horizontal">
+                                <div class="preview"></div>
+                                <div style="display: none" class="form-group plain_html"><textarea rows="50" class="form-control"></textarea></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    <div class="row">
         <!-- Form Fields List and Form elements -->
         <div class="col-sm-8">
-            <div class="card">
-                <div class="card-body">
-                    <div class="input-group input-group-lg">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroup-sizing-lg" style="width:120px;">Form Title</span>
-                        </div>
-                        <input type="text" id="formTitle" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
-                    </div>
-                    <div class="input-group input-group-lg">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroup-sizing-lg1" style="width:120px;">Description</span>
-                        </div>
-                        <input type="text" id="formTitle" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm1">
-                    </div>
-                </div>
-
-                <div class="card-body" style="border:2px solid #f0f0f0">
-                    <strong>First Name, Middle Name, Last Name and Email Address</strong>
-                        are always collected by every form.
-                    </<strong>
-                </div>
-                <div class="card-body bg-primary" id="dropdownElsList">
-                </div>
-            </div>
-        </div>
-        <!-- Add a Fields Block -->
-        <div class="col-sm-4 " style="min-height:450px;">
-            <div class="card">
-                <h5 class="card-title bg-primary" style="margin:0px; padding:15px;">Add A Field</h5>
-                <div class="card-body">
-                    <h6 class="card-title text-secondary">Profile Fields</h6>
-                    <div id="profileFields"></div>
-                </div>
-                <div class="card-body">
-                    <h6 class="card-title text-secondary">Basic Fields</h6>
-                    <div id="basicFields"></div>
-                </div>
-            </div>
+            
         </div>
     </div>
-    <script>
-    let personalFieldsList = [{title:"Phone Number", tag:"mobile_no", icon:"fa fa-phone", isRequired:false},
-                              {title:"Address", tag:"address", icon:"fa fa-address-card", isRequired:false},
-                              {title:"Birthday", tag:"dob", icon:"fa fa-birthday-cake", isRequired:false},
-                              {title:"Grade", tag:"grade_id", icon:"fa fa-apple", isRequired:false},
-                              {title:"Medical Note", tag:"medical_note", icon:"fa fa-heartbeat", isRequired:false},
-                              {title:"Marital Status", tag:"marital_status", icon:"fa fa-users", isRequired:false},
-                              {title:"Life Stage", tag:"life_stage", icon:"fa fa-user", isRequired:false},
-                              {title:"Gender", tag:"gender",  icon:"fa fa-mars-double", isRequired:false}
-                            ];
-    let basicFieldsList = [{fieldTitle:"Text", inputType:"text", title:"", description:"", isRequire:false},
-                              {fieldTitle:"Paragraph", inputType:"textarea", title:"", description:"", isRequire:false},
-                              {fieldTitle:"Checkbox", inputType:"checkbox", title:"", description:"", isRequire:false},
-                              {fieldTitle:"Date", inputType:"data", title:"", description:"", isRequire:false},
-                              {fieldTitle:"Number", inputType:"number", title:"", description:"", isRequire:false},
-                              {fieldTitle:"Section Heading", inputType:null, title:"", description:""}
-                            ];
-    $(function () {
-        updateProfileFields();
-        updateBasicFields();
-    }); 
-
-    function updateProfileFields(){
-        let profileEls = personalFieldsList.map(function(item, index){
-                            return `<p id="personalField-${index}" class="form-settings-fields">
-                                        <i class="${item.icon}"></i> ${item.title}
-                                    </p>`
-                        });
-        $("#profileFields").html(profileEls);
-    }
-    function updateBasicFields(){
-        let profileEls = basicFieldsList.map(function(item, index){
-                            return `<p id="basicField-${index}" class="form-settings-fields">
-                                        ${item.fieldTitle}
-                                    </p>`
-                        });
-        $("#basicFields").html(profileEls);
-    }
-
-</script>
+    <script src="{{ URL:: asset('assets/theme/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+    <script src="{{ URL:: asset('js/form-build.js')}}"></script>
 @endsection
