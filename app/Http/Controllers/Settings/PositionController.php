323cbadae2440f06faf09f93036058ae319fc134
Sathish Kumar S <sathish.k@glowtouch.com>
2019-10-30 18:07:47 +0530

    Member update position done

--- app/Http/Controllers/Settings/PositionController.php
+++ app/Http/Controllers/Settings/PositionController.php
@@ -17,6 +17,7 @@ use App\Models\MasterLookupData;
 use App\Models\SchedulingUser;
 use App\Models\CommTemplate;
 use App\User;
+use DB;
 
 class PositionController extends Controller
 {
@@ -29,33 +30,34 @@ class PositionController extends Controller
         $this->authUserId = $this->userSessionData['umId'];
     }
 
-    public function positionIndex(){
+    public function positionIndex()
+    {
         $data['title'] = $this->browserTitle . " - position List";
         return view('settings.position.index', $data);
     }
 
-    public function getPositionList(){
+    public function getPositionList()
+    {
         $result = array();
-        $whereArray=array('orgId'=>$this->orgId);
-        $positions = Position::selectPositionDetail($whereArray,null,null,null,null,null)->get();
-        
+        $whereArray = array('orgId' => $this->orgId);
+        $positions = Position::selectPositionDetail($whereArray, null, null, null, null, null)->get();
+
         $i = 1;
         foreach ($positions as $position) {
             $row = array();
-            
+
             $row[] = $position->id;
             $row[] = $position->name;
             //showConfirm
-            $button_html = '<a  onclick="edit_position('.$position->id.')"  data-toggle="tooltip"   href="#"  data-original-title="Edit"><i class="fa fa-edit"></i></a>
-                                <a onclick="position_data_delete('.$position->id.')"   href="#"><i class="fa fa-trash"></i></a>';
-            
+            $button_html = '<a  onclick="edit_position(' . $position->id . ')"  data-toggle="tooltip"   href="#"  data-original-title="Edit"><i class="fa fa-edit"></i></a>
+                                <a onclick="position_data_delete(' . $position->id . ')"   href="#"><i class="fa fa-trash"></i></a>';
+
             $row[] = $button_html;
             $result[] = $row;
         }
 
         //return Datatables::of($result)->rawColumns([6])->make(true);
         return Datatables::of($result)->escapeColumns(['id'])->make(true);
-
     }
 
     /**
@@ -64,66 +66,89 @@ class PositionController extends Controller
      * @Added by : Sathish
      * @Added Date : Nov 07, 2018
      */
-    public function getPositionById(Request $request) {
+    public function getPositionById(Request $request)
+    {
         $getPositionById = Position::find($request->get('positionID'));
         return $getPositionById;
-        
     }
 
-    public function storeOrUpdatePosition(Request $request){
+    public function storeOrUpdatePosition(Request $request)
+    {
         $PositionFormData = $request->except('hidden_positionID');
         $PositionFormData['createdBy'] = $this->authUserId;
         $PositionFormData['orgId'] = $this->orgId;
-        unset($PositionFormData['hidden_positionID'],$PositionFormData['_token']);
-        if($request->get('hidden_positionID') > 0){
+        unset($PositionFormData['hidden_positionID'], $PositionFormData['_token']);
+        if ($request->get('hidden_positionID') > 0) {
             unset($PositionFormData['createdBy']);
             $PositionFormData['updatedBy'] = $this->authUserId;
             $whereArray = array('id' => $request->get('hidden_positionID'));
-            $updateDetails = Position::updatePosition($PositionFormData,$whereArray);
-            return "updated";            
-        }else{
+            $updateDetails = Position::updatePosition($PositionFormData, $whereArray);
+            return "updated";
+        } else {
             $insertDetails = Position::create($PositionFormData);
-            if($insertDetails->id > 0){
+            if ($insertDetails->id > 0) {
                 return "inserted";
-            }else{
+            } else {
                 return 0;
             }
         }
     }
- 
+
     /**
      * @Function name : deletePositionById
      * @Purpose : Delete from Position
      * @Added by : Sathish
      * @Added Date : Nov 07, 2018
      */
-    public function deletePositionById(Request $request) {
-        
-        $whereArray=array('id'=>$request->get('positionId'));
-        $updateAHDeletedStatus = array('deleted_at' => now(),'deletedBy' => $this->authUserId);  
-        Position::crudPosition($whereArray,null,null,null,null,$updateAHDeletedStatus,null,null);       
-   }
+    public function deletePositionById(Request $request)
+    {
 
-   /**
+        $whereArray = array('id' => $request->get('positionId'));
+        $updateAHDeletedStatus = array('deleted_at' => now(), 'deletedBy' => $this->authUserId);
+        Position::crudPosition($whereArray, null, null, null, null, $updateAHDeletedStatus, null, null);
+    }
+
+    /**
      * @Function name : storeOrUpdateUserHasPos
      * @Purpose : Delete from Position
      * @Added by : Sathish
      * @Added Date : Nov 07, 2018
      */
-    public function storeOrUpdateUserHasPos(Request $request) {
-        
-        $user_pos_id = $request->user_pos_id;
+    public function storeOrUpdateUserHasPos(Request $request)
+    {
 
-        $exp_user_pos_id = explode(",",$user_pos_id);
-        //dd($exp_user_pos_id);
-        foreach($exp_user_pos_id as $exp_user_pos_id_value){
-            $whereUHPArray = array('user_id'=>$request->user_id,'position_id'=>$exp_user_pos_id_value);
+        DB::beginTransaction();
 
-            $arrayUHPUpdate = array('user_id'=>$request->user_id,'position_id'=>$exp_user_pos_id_value);
-            UserHasPosition::updateOrCreate($whereUHPArray, $arrayUHPUpdate);
+        try {
+            $user_pos_id = $request->user_pos_id;
+            $user_id = $request->user_id;
+
+            $exp_user_pos_id = explode(",", $user_pos_id);
+            //dd($exp_user_pos_id);
+            //First update not in to null(deleted)
+            $whereNotInUHPUpdArray = array('position_id' => $exp_user_pos_id);
+            $update_details_uhp = array('deleted_at' => now(), 'deletedBy' => $this->authUserId);
+            $whereUHPUpdArray = array('user_id' => $user_id);
+            UserHasPosition::crudUserHasPosition($whereUHPUpdArray, null, $whereNotInUHPUpdArray, null, null, $update_details_uhp, null, null);
+
+            //Insert or update
+            foreach ($exp_user_pos_id as $exp_user_pos_id_value) {
+                $whereUHPArray = array('user_id' => $request->user_id, 'position_id' => $exp_user_pos_id_value);
+
+                $arrayUHPUpdate = array('orgId' => $this->orgId, 'createdBy' => $this->authUserId, 'user_id' => $request->user_id, 'position_id' => $exp_user_pos_id_value);
+                UserHasPosition::updateOrCreate($whereUHPArray, $arrayUHPUpdate);
+            }
+            DB::commit();
+            return response()->json(['result_code' => 1, 'message' => 'Position Updated Successfully.'], 200);
+        } catch (\Illuminate\Database\QueryException $e) {
+            DB::rollback();
+            return response()->json(['result_code' => 0, 'message' => "Sorry , Changes not updated. Please try again!!!!!"], 200);//$e->getMessage()
+            // something went wrong with the transaction, rollback
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['result_code' => 0, 'message' => "Sorry , Changes not updated. Please try again!!!!!"], 200);
+            return Redirect::back()->withErrors($e->getMessage());
         }
-        
         //return ($request->all());
     }
 }
-
